33a93401686104bb0fd95cdbdf22e928dffbecb7
ofriznz-web <ofriznz@gmail.com>
2025-11-27 12:52:30 +0200

    Update histogram_utils.py

--- histogram_utils.py
+++ histogram_utils.py
@@ -1,20 +1,8 @@
-import matplotlib.pyplot as plt
-
-def build_histogram(data):
-    histogram = {}
-    for item in data:
-        if item in histogram:
-            histogram[item] += 1
-        else:
-            histogram[item] = 1
-    return histogram
-
-def plot_histogram(histogram):
-    x_values = list(histogram.keys())
-    y_values = list(histogram.values())
-
-    plt.bar(x_values, y_values)
-    plt.xlabel('Items')
-    plt.ylabel('Frequency')
-    plt.title('Histogram')
-    plt.show()
+def read_file(file_path):
+  file_content = open(file_path)
+  lines = file_content.readlines()
+  file_content.close()
+  long_string = ""
+  for line in lines:
+    long_string += line.strip()
+  return long_string
